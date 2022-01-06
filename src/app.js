a1866ee165b5796680f930709397800388fea85e
Angelo Geant Gaviola <yomama@example.com>
2022-01-06 20:53:09 +0800

    set the views and public to relative paths

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const allRoute = require("./routes/index");
+const path = require("path");
 
 // To parse multipart forms with extended html characters
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static("./public"));
-app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Import all routes
